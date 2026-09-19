0b77b4ec75e3856c64b2969608b765f24c38d3b5
Sebastián Federico <fscfede@gmail.com>
2026-09-19 04:09:26 -0300

    test: cover explicit SSH override in Git metadata runner

--- codex-rs/git-utils/src/info.rs
+++ codex-rs/git-utils/src/info.rs
@@ -1116,7 +1116,7 @@ mod tests {
                 "config --null --get core.fsmonitor".to_string(),
                 "config --null --type=bool --fixed-value --get core.fsmonitor /tmp/fsmonitor-helper"
                     .to_string(),
-                format!("-c {disabled_hooks} -c core.fsmonitor=false status --porcelain"),
+                format!("-c {disabled_hooks} -c core.fsmonitor=false -c core.sshCommand= status --porcelain"),
             ]
         );
     }
@@ -1204,7 +1204,7 @@ mod tests {
             vec![
                 "config --null --get core.fsmonitor".to_string(),
                 "version --build-options".to_string(),
-                format!("-c {disabled_hooks} -c core.fsmonitor=true status --porcelain"),
+                format!("-c {disabled_hooks} -c core.fsmonitor=true -c core.sshCommand= status --porcelain"),
             ]
         );
     }
